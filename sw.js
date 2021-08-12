5eabcce4def0c6cc4683967ba94e07834a0a4ba5
juanxd01 <80122507+juanxd01@users.noreply.github.com>
2021-08-11 22:07:06 -0500

    fdfd

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ;
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_programador_fitness',
+const CACHE_NAME = 'v1',
   urlsToCache = [
     './',
     'https://fonts.googleapis.com/css?family=Raleway:400,700',
@@ -9,7 +9,6 @@ const CACHE_NAME = 'v1_cache_programador_fitness',
     'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
     './style.css',
     './script.js',
-    './img/ProgramadorFitness.png',
     './img/favicon.png'
   ]
 
